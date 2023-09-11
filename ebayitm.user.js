53dfcaf5ee0fe1a365d239c0f98698f7813385fb
pnppl <pnpplpnppl@gmail.com>
2023-09-11 16:40:06 -0400

    Enumerated int'l site matches
    Bromite userscript engine sucks

--- ebayitm.user.js
+++ ebayitm.user.js
@@ -1,10 +1,27 @@
 // ==UserScript==
 // @name        eBay easy item ID
 // @namespace   pnppl
-// @match       http*://www.ebay.*/itm/*
-// @match       http*://ebay.*/itm/*
+// @match       https://www.ebay.com/itm/*
+// @match       https://www.ebay.at/itm/*
+// @match       https://www.ebay.ca/itm/*
+// @match       https://www.ebay.ch/itm/*
+// @match       https://www.ebay.co.nz/itm/*
+// @match       https://www.ebay.co.uk/itm/*
+// @match       https://www.ebay.com.au/itm/*
+// @match       https://www.ebay.com.hk/itm/*
+// @match       https://www.ebay.com.my/itm/*
+// @match       https://www.ebay.com.sg/itm/*
+// @match       https://www.ebay.de/itm/*
+// @match       https://www.ebay.es/itm/*
+// @match       https://www.ebay.fr/itm/*
+// @match       https://www.ebay.ie/itm/*
+// @match       https://www.ebay.in/itm/*
+// @match       https://www.ebay.it/itm/*
+// @match       https://www.ebay.nl/itm/*
+// @match       https://www.ebay.ph/itm/*
+// @match       https://www.ebay.pl/itm/*
 // @grant       none
-// @version     1.1
+// @version     1.11
 // @author      pnppl
 // @description Puts the item ID under the title with buttons for copying it and the cleaned URL
 // @license     MIT
@@ -13,7 +30,7 @@
 const url = window.location.href;
 const reItm = /.*\/([0-9]{12})/;
 const itm = url.match(reItm)[1];
-const reTLD = /^https?:\/\/(?:www.)?(ebay\.[a-z]{2,3}(?:\.[a-z]{2,3})?)/i;
+const reTLD = /^https:\/\/(?:www.)?(ebay\.[a-z]{2,3}(?:\.[a-z]{2,3})?)/i;
 const tld = url.match(reTLD)[1];
 const cleanURL = `https://${tld}/itm/${itm}`;
 const titleMob = document.querySelector("#vi-frag-app-title");
