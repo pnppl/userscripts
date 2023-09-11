5b201ec31dc1a05038fb2eec73764f8e1b995024
pnppl <pnpplpnppl@gmail.com>
2023-09-11 16:13:41 -0400

    Added desktop and international support

--- ebayitm.user.js
+++ ebayitm.user.js
@@ -1,30 +1,38 @@
 // ==UserScript==
-// @name        eBay mobile easy item ID
+// @name        eBay easy item ID
 // @namespace   pnppl
-// @match       https://www.ebay.com/itm/*
+// @match       http*://www.ebay.*/itm/*
+// @match       http*://ebay.*/itm/*
 // @grant       none
-// @version     1.01
+// @version     1.1
 // @author      pnppl
 // @description Puts the item ID under the title with buttons for copying it and the cleaned URL
 // @license     MIT
 // ==/UserScript==
 
 const url = window.location.href;
-const re = /.*\/([0-9]{12})/;
-const itm = url.match(re)[1];
-const cleanURL = `https://www.ebay.com/itm/${itm}`;
-const title = document.querySelector("#vi-frag-app-title");
+const reItm = /.*\/([0-9]{12})/;
+const itm = url.match(reItm)[1];
+const reTLD = /^https?:\/\/(?:www.)?(ebay\.[a-z]{2,3}(?:\.[a-z]{2,3})?)/i;
+const tld = url.match(reTLD)[1];
+const cleanURL = `https://${tld}/itm/${itm}`;
+const titleMob = document.querySelector("#vi-frag-app-title");
+const titleDesk = document.querySelector(".x-item-title");
 const newHTML = `               \
     <style>                     \
         button:focus.userjs {   \
             background:lime;    \
         }                       \
     </style>                    \
-    <div class="vi-sellerinfo"> \
+    <div>                       \
         <p>                     \
             ${itm} <button class="userjs" onclick="navigator.clipboard.writeText('${itm}')">📋</button> <button class="userjs" onclick="navigator.clipboard.writeText('${cleanURL}')">🔗</button> \
         </p>                    \
     </div>                      \
 `;
 
-title.insertAdjacentHTML("afterend", newHTML);
+if (titleMob == null) {
+    titleDesk.insertAdjacentHTML("afterend", newHTML);
+} else {
+    titleMob.insertAdjacentHTML("afterend", newHTML);
+}
